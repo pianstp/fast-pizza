f9698eb8a6612717f96bcd83f861433630d7216f
Pianistpromise <onyebuchipromise44@gmail.com>
2025-12-25 13:22:07 +0100

    introduce tailwind

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+ //prettier.config.js
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+//  export default {
+//   plugins: ["prettier-plugin-tailwindcss"],
+//    singleQuote: true,
+//  };
+
+
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+};
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">jonas</div>;
+}
+
+export default Username;
